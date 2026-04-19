2ea12d5b6fdc1501b6f58e542bf640ee08b811ab
mrheichberger <mrheichberger@gmail.com>
2026-04-19 15:02:17 -0400

    backup in case

--- motor_control_program/main.c
+++ motor_control_program/main.c
@@ -5,43 +5,118 @@
 #include <stdbool.h>
 #include "uart_comm.h"
 
-#define COUNTS_PER_DETENT 4
-#define DETENTS_PER_SLOT 1
-#define UART_TX_PIN 16
-#define UART_RX_PIN 17
 
+#define COUNTS_PER_SLOT 16
+#define MOVE_TIMEOUT_MS 15000
 
 int32_t move_geneva_slots(motor_driver_t *m, encoder_t *enc, int32_t requested_slots)
 {
-    int32_t start_pos = encoder_get_position(enc);
-    int32_t start_det = start_pos / COUNTS_PER_DETENT;
-    int32_t start_slot = start_det / DETENTS_PER_SLOT;
+    if (requested_slots <= 0) {
+        return 0;
+    }
 
-    int32_t target_slot = start_slot + requested_slots;
+    int32_t start_counts = encoder_get_position(enc);
+    int32_t target_counts = start_counts + (requested_slots * COUNTS_PER_SLOT);
+
+    printf("move_geneva_slots() START\n");
+    printf("  start_counts    = %ld\n", (long)start_counts);
+    printf("  requested_slots = %ld\n", (long)requested_slots);
+    printf("  target_counts   = %ld\n", (long)target_counts);
 
     motor_forward_full(m);
 
-    while (1) {
-        int32_t pos = encoder_get_position(enc);
-        int32_t det = pos / COUNTS_PER_DETENT;
-        int32_t current_slot = det / DETENTS_PER_SLOT;
+    int timeout = 0;
 
-        if (current_slot >= target_slot) {
+    while (1) {
+        int32_t current_counts = encoder_get_position(enc);
+
+        if (timeout % 500 == 0) {
+            printf("  loop: current_counts=%ld target_counts=%ld\n",
+                   (long)current_counts, (long)target_counts);
+        }
+
+        if (current_counts >= target_counts) {
+            printf("  target reached\n");
             break;
         }
 
         sleep_ms(1);
+        timeout++;
+
+        if (timeout > MOVE_TIMEOUT_MS) {
+            printf("  TIMEOUT in encoder loop\n");
+            break;
+        }
     }
 
     motor_stop(m);
     sleep_ms(10);
 
-    int32_t end_pos = encoder_get_position(enc);
-    int32_t end_det = end_pos / COUNTS_PER_DETENT;
-    int32_t end_slot = end_det / DETENTS_PER_SLOT;
+    int32_t end_counts = encoder_get_position(enc);
+    int32_t actual_slots_moved = (end_counts - start_counts) / COUNTS_PER_SLOT;
 
-    return end_slot - start_slot;
+    printf("move_geneva_slots() END\n");
+    printf("  end_counts         = %ld\n", (long)end_counts);
+    printf("  actual_slots_moved = %ld\n", (long)actual_slots_moved);
+
+    return actual_slots_moved;
 }
+
+/*
+   int32_t move_geneva_slots(motor_driver_t *m, encoder_t *enc, int32_t requested_slots)
+{
+    if (requested_slots <= 0) {
+        return 0;
+    }
+
+    int32_t start_slot = encoder_get_slot(enc, COUNTS_PER_SLOT, SLOT_THRESHOLD);
+    int32_t target_slot = start_slot + requested_slots;
+
+    printf("move_geneva_slots() START\n");
+    printf("  start_slot      = %ld\n", (long)start_slot);
+    printf("  requested_slots = %ld\n", (long)requested_slots);
+    printf("  target_slot     = %ld\n", (long)target_slot);
+
+    motor_forward_full(m);
+
+    int timeout = 0;
+
+    while (1) {
+        int32_t current_slot = encoder_get_slot(enc, COUNTS_PER_SLOT, SLOT_THRESHOLD);
+
+        if (timeout % 500 == 0) {
+            int32_t pos = encoder_get_position(enc);
+            printf("  loop: pos=%ld current_slot=%ld target_slot=%ld\n",
+                   (long)pos, (long)current_slot, (long)target_slot);
+        }
+
+        if (current_slot >= target_slot) {
+            printf("  target reached\n");
+            break;
+        }
+
+        sleep_ms(1);
+        timeout++;
+
+        if (timeout > MOVE_TIMEOUT_MS) {
+            printf("  TIMEOUT in encoder loop\n");
+            break;
+        }
+    }
+
+    motor_stop(m);
+    sleep_ms(10);
+
+    int32_t end_slot = encoder_get_slot(enc, COUNTS_PER_SLOT, SLOT_THRESHOLD);
+    int32_t actual_slots_moved = end_slot - start_slot;
+
+    printf("move_geneva_slots() END\n");
+    printf("  end_slot           = %ld\n", (long)end_slot);
+    printf("  actual_slots_moved = %ld\n", (long)actual_slots_moved);
+
+    return actual_slots_moved;
+}
+*/
 
 // ---------------- motors ----------------
 motor_driver_t m1 = {
@@ -166,43 +241,28 @@ int main() {
     encoder_init(&enc7);
     encoder_init(&enc8);
 
-    uart_comm_init();
+    sleep_ms(2000);  // gives COM port time to connect
 
-    int32_t requested_motor = 0;
-int32_t requested_slots = 0;
-bool motor_received = false;
+    printf("Motor 3 encoder calibration test\n");
+    printf("Resetting encoder 3 to zero...\n");
 
-while (1) {
-    if (!motor_received) {
-        if (uart_comm_read_int(&requested_motor)) {
-            motor_received = true;
-        }
-    } else {
-        if (uart_comm_read_int(&requested_slots)) {
-            if (requested_motor >= 1 && requested_motor <= 8) {
-                int index = requested_motor - 1;
+    encoder_reset(&enc3);
 
-                int32_t actual_slots_moved = move_geneva_slots(
-                    motors[index],
-                    encoders[index],
-                    requested_slots
-                );
+    printf("Starting motor 3 now\n");
+    motor_forward_full(&m3);
 
-                uart_comm_send_int(actual_slots_moved);
-            } else {
-                uart_comm_send_int(-1);
-            }
-
-            motor_received = false; // ready for next motor command
-        }
+    while (1) {
+        int32_t pos = encoder_get_position(&enc3);
+        printf("Encoder count: %ld\n", (long)pos);
+        sleep_ms(200);
     }
+}
+    */
 
-    sleep_ms(5);
-}
-}
-*/
 int main() {
     stdio_init_all();
+
+    printf("Starting motor control + UART debug program\n");
 
     motor_init(&m1);
     motor_init(&m2);
@@ -227,19 +287,59 @@ int main() {
     int32_t requested_motor = 0;
     int32_t requested_slots = 0;
     bool motor_received = false;
-
+/*
     while (1) {
+        //--------------
+        //just for debug start here 
+  printf("TURNING ALL MOTORS ON\n");
+
+    // Turn all motors ON
+    for (int i = 0; i < 8; i++) {
+        motor_forward_full(motors[i]);
+    }
+
+    sleep_ms(10000); // 10 seconds ON
+
+    printf("TURNING ALL MOTORS OFF\n");
+
+    // Turn all motors OFF
+    for (int i = 0; i < 8; i++) {
+        motor_stop(motors[i]);
+    }
+
+    sleep_ms(5000); // 5 seconds OFF
+}
+//-------------------
+        //just for debug end here
+        */
         if (!motor_received) {
             if (uart_comm_read_int(&requested_motor)) {
-                printf("Got motor: %ld\n", (long)requested_motor);
+                printf("UART received motor number: %ld\n", (long)requested_motor);
                 motor_received = true;
             }
         } else {
             if (uart_comm_read_int(&requested_slots)) {
-                printf("Got slots: %ld\n", (long)requested_slots);
+                printf("UART received slots: %ld\n", (long)requested_slots);
 
-                uart_comm_send_int(99);
-                printf("Sent reply: 99\n");
+                if (requested_motor >= 1 && requested_motor <= 8) {
+                    int index = requested_motor - 1;
+
+                    printf("About to move motor %ld (index %d) by %ld slots\n",
+                           (long)requested_motor, index, (long)requested_slots);
+
+                    int32_t actual_slots_moved = move_geneva_slots(
+                        motors[index],
+                        encoders[index],
+                        requested_slots
+                    );
+
+                    printf("Sending actual_slots_moved back over UART: %ld\n",
+                           (long)actual_slots_moved);
+                    uart_comm_send_int(actual_slots_moved);
+                } else {
+                    printf("Invalid motor number: %ld\n", (long)requested_motor);
+                    uart_comm_send_int(-1);
+                }
 
                 motor_received = false;
             }
@@ -247,4 +347,5 @@ int main() {
 
         sleep_ms(5);
     }
-}
+        
+
